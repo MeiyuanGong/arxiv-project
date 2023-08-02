4285b80a44a47f7907c67397aa136292d62e1093
MeiyuanGong <109795706+MeiyuanGong@users.noreply.github.com>
2023-08-02 21:04:26 +0100

    Update get_new_tittle.py

--- get_new_tittle.py
+++ get_new_tittle.py
@@ -9,6 +9,22 @@ rl = BeautifulSoup(papers, "html.parser")
 rl_papers = rl.find_all("p", {"class": "title is-5 mathjax"})
 titles_list = [paper.get_text().strip() for paper in rl_papers]
 
+def ger_info(rl):
+    rl_divs = rl.find_all("div", {"class": "tags is-inline-block"})
+    divs_list = [div.get_text().strip() for div in rl_divs]
+
+    rl_papers = rl.find_all("p", {"class": "title is-5 mathjax"})
+    titles_list = [paper.get_text().strip() for paper in rl_papers]
+
+    rl_authors = rl.find_all("p", {"class": "authors"})
+    authors_list = [author.get_text().strip() for author in rl_authors]
+
+    rl_abstracts = rl.find_all("span", {"class": "abstract-full has-text-grey-dark mathjax"})
+    abstracts_list = [abstract.get_text().strip() for abstract in rl_abstracts]
+
+    
+    return titles_list, authors_list, abstracts_list, divs_list
+
 # Save titles to a text file (one title per line)
 with open("rl_titles.txt", "w", encoding="utf-8") as file:
     for title in titles_list:
