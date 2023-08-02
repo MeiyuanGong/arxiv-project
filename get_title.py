5b2981e9eef3b566397bbf8fb65ffd0f1d6c8149
MeiyuanGong <109795706+MeiyuanGong@users.noreply.github.com>
2023-08-02 18:37:32 +0100

    Add files via upload

---
+++ get_title.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+response = requests.get("https://arxiv.org/search/?query=reinforcement+learning&searchtype=all&source=header")
+papers = response.text
+rl = BeautifulSoup(papers, "html.parser")
+rl_papers = rl.find_all("p", {"class": "title is-5 mathjax"})
+# a = rl_papers[0].find("p", {"class": "title is-5 mathjax"})
+titles_list = [paper.get_text().strip() for paper in rl_papers]
+
+# Save titles to a text file (one title per line)
+with open("rl_titles.txt", "w", encoding="utf-8") as file:
+    for title in titles_list:
+        file.write(title + "\n")
+
+# Print the titles to the console
+for title in titles_list:
+    print(title)
